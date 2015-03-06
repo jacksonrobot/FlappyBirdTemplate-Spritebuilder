c4e432ef771a697e17277f7532e437d6c43da4b7
金凯 <jinxiaolin@jinkaideMacBook-Pro.local>
2015-03-06 23:09:49 +0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,9 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"character"];
+    [physicsNode addChild:character];
+
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +18,23 @@
 }
 
 // put new methods here
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
+// this will get called every time the player touches the screen
+    [character flap];
+}
 
+-(void) doSomething
+{
+    NSLog(@"Hello World!");
+}
+-(int) doSomethingWithAString: (NSString*) myString
+{
+    //returns the length of the string times ten
+    return [myString length] * 10;
+}
+-(NSArray *) addThisStringToAnArray:(NSString*) firstString andThisString:(NSString*) secondString andAlsoThisString:(NSString*) thirdString
+{
+    return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
+}
+-(type I return)nameOfMethod: (type of first parameter) firstParameter;continuationOfMethodName: (type of second parameter) secondParameter;
 @end
