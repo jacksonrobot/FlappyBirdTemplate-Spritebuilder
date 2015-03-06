52a28b0c73a02415f41d61bbdc296e5983227aea
金凯 <jinxiaolin@jinkaideMacBook-Pro.local>
2015-03-06 23:26:41 +0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,6 +10,7 @@
     character = (Character*)[CCBReader load:@"character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
