52b6e741aa1c9c2c7d29b7c9655dc0f3ec501726
金凯 <jinxiaolin@jinkaideMacBook-Pro.local>
2015-03-07 12:00:23 +0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -62,25 +62,7 @@
     _bushes = @[_bush1, _bush2];
     _clouds = @[_cloud1, _cloud2];
    
-    _parallaxBackground = [CCParallaxNode node];
-    [_parallaxContainer addChild:_parallaxBackground];
-    
-    // Note that the bush ratio is larger than the cloud
-    _bushParallaxRatio = ccp(0.9, 1);
-    _cloudParallaxRatio = ccp(0.5, 1);
-    
-    for (CCNode *bush in _bushes) {
-        CGPoint offset = bush.position;
-        [self removeChild:bush];
-        [_parallaxBackground addChild:bush z:0 parallaxRatio:_bushParallaxRatio positionOffset:offset];
-    }
-    
-    for (CCNode *cloud in _clouds) {
-        CGPoint offset = cloud.position;
-        [self removeChild:cloud];
-        [_parallaxBackground addChild:cloud z:0 parallaxRatio:_cloudParallaxRatio positionOffset:offset];
-    }    
-    for (CCNode *ground in _grounds) {
+        for (CCNode *ground in _grounds) {
         // set collision txpe
         ground.physicsBody.collisionType = @"level";
         ground.zOrder = DrawingOrderGround;
@@ -190,7 +172,6 @@
         if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
             ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
         }
-        _parallaxBackground.position = ccp(_parallaxBackground.position.x - (character.physicsBody.velocity.x * delta), _parallaxBackground.position.y);
         
         // loop the bushes
         for (CCNode *bush in _bushes) {
